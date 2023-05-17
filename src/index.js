7c650e48a7bf14142f064759ae0ee2e509582400
UserGitHub37 <bum37@mail.ru>
2023-05-18 00:20:11 +0300

    refactor: do code refactoring

--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { bindActionCreators, legacy_createStore } from 'redux';
-import * as actions from './actions';
+import { legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
 import './index.css';
 import reducer from './reducer';
-// import App from './App';
 
 const store = legacy_createStore(reducer);
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-// const incDispatch = bindActionCreator(inc, dispatch);
-// const decDispatch = bindActionCreator(dec, dispatch);
-// const rndDispatch = bindActionCreator(rnd, dispatch);
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
